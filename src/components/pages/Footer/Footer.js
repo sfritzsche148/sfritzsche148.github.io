58204fbf7f1ec8681395f5b136576313ce617792
Sebastian Fritzsche <sebifr58@gmx.de>
2022-06-18 20:19:55 +0200

    update footer

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -4,25 +4,15 @@ import { Link } from 'react-router-dom';
 import {
   FaInstagram,
   FaLinkedin,
+  FaGithub
 } from 'react-icons/fa';
-import { TiCodeOutline } from 'react-icons/ti';
+import { TiCodeOutline, TiMail } from 'react-icons/ti';
 
 function Footer() {
   return (
     <div className='footer-container'>
       <div className='footer-links'>
         <div className='footer-link-wrapper'>
-        </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>Social Media</h2>
-            <a className='link' href="https://www.instagram.com/s_fritzsche_/">Instagram</a>
-            <a className='link' href="https://www.linkedin.com/in/sebastian-fritzsche-1393281b1/">LinkedIn</a>
-          </div>
-          <div className='footer-link-items'>
-            <h2>Kontakt</h2>
-            <a className='link' href="mailto: sfritzsche148@gmail.com">E-Mail: sfritzsche148@gmail.com</a>
-          </div>
         </div>
       </div>
       <section className='social-media'>
@@ -36,12 +26,20 @@ function Footer() {
           <small className='website-rights'>Sebastian Fritzsche © 2022</small>
           <div className='social-icons'>
           <a className='social-icon-link'
-           href="https://www.instagram.com/s_fritzsche_/">
+           href="https://www.instagram.com/sebastianfritzsche_/">
               <FaInstagram />
               </a>
               <a className='social-icon-link'
                href="https://www.linkedin.com/in/sebastian-fritzsche-1393281b1/">
               <FaLinkedin />
+              </a>
+              <a className='social-icon-link'
+               href="mailto: sfritzsche148@gmail.com">
+              <TiMail />
+              </a>
+              <a className='social-icon-link'
+               href="https://github.com/sfritzsche148">
+              <FaGithub />
               </a>
           </div>
         </div>
--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -6,8 +6,6 @@ function Home() {
     <div className='home'>
       <h1 className='textheader'>Hi,</h1>
       <h1 className='textheader1'>ich bin Sebastian</h1>
-      
-
     </div>
   )
 }
