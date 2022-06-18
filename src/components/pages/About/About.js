9aa932d8f3a1df8a7865d915dc0dcfd2a6522e62
Sebastian Fritzsche <sebifr58@gmx.de>
2022-06-18 21:42:55 +0200

    update about page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './About.css';
+import { FaHtml5, FaReact, FaWindows, FaUbuntu } from 'react-icons/fa';
+import { DiCss3, DiMysql } from 'react-icons/di';
+import { IoLogoJavascript } from 'react-icons/io';
 
 function About() {
 
@@ -11,50 +14,93 @@ function About() {
                 src="/images/Profilbild_3.jpg"
                 alt=""
              />
-            <p className='text'>
-                Hi, ich bin Sebastian
-            </p>
-            <p className='text'>und Schüler der 9. Klasse</p>
-            <p className='description__text'>an einer bayrischen Realschule.</p>
-            <p className='text'>
-            Ich liebe Sport
-            </p>
-            <p className='text'>& alles was mit Technik zu tun hat.</p>
-            <h2 className='second__title'>Kompetenzen</h2>
-            <li className='about__list'>Designbibliotheken</li>
-            <li className='about__list'>Lieferung hochwertiger und moderner Produkte</li>
-            <li className='about__list'>Toolset: Html, Css, Javascript, Reactjs</li>
-            <h2 className='second__title'>Hobbies</h2>
-            <h3 className='third__title'>programmieren</h3>
-            <img 
-                className='programm__bild'
-                src="/images/programmieren.jpg"
-                alt="hund"
-             />
-            <h3 className='third__title'>mein Hund</h3>
-            <img 
-                className='hobbie__bild'
-                src="/images/rocky.jpg"
-                alt="hund"
-             />
-             <h3 className="third__title">Tennis</h3>
-             <img 
-                className='hobbie__bild'
-                src="/images/tennis.jpg"
-                alt="tennis"
-             />
-            <h3 className='third__title'>Mountainbiken</h3>
-            <img 
-                className='hobbie__bild'
-                src="/images/bike.jpg"
-                alt="bike"
-             />
-             <h3 className='third__title'>Reisen</h3>
-             <img 
-                className='hobbie__bild'
-                src="/images/strand.jpg"
-                alt="strand"
-             />
+             <div>
+                <p className='text'>
+                    Hi, ich bin Sebastian
+                </p>
+                <p className='text'>und Schüler der 9. Klasse</p>
+                <p className='description__text'>an einer bayrischen Realschule.</p>
+                <p className='text'>
+                Ich liebe Sport
+                </p>
+                <p className='text'>& alles was mit Technik zu tun hat.</p>
+            </div>
+            <div className='skillset'>
+                <h2 className='second__title'>Skillset</h2>
+                <div className='skillsetlist'>
+                    <ul>
+                        <li>
+                            <div className='skillsetcontent'>
+                                <h3 className='third__title'>Frontend</h3>
+                                <a className='skillicon' href="https://de.wikipedia.org/wiki/HTML5">
+                                    <FaHtml5 />
+                                </a>
+                                <a className='skillicon' href="https://de.wikipedia.org/wiki/Cascading_Style_Sheets">
+                                    <DiCss3 />
+                                </a>
+                                <a className='skillicon' href="https://de.wikipedia.org/wiki/JavaScript">
+                                    <IoLogoJavascript />
+                                </a>
+                                <a className='skillicon' href="https://reactjs.org/">
+                                    <FaReact />
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='skillsetcontent'>
+                                <h3 className='third__title'>Betriebssysteme</h3>
+                                <a className='skillicon' href="https://www.microsoft.com/de-de/windows?r=1">
+                                    <FaWindows />
+                                </a>
+                                <a className='skillicon' href="https://ubuntu.com/">
+                                    <FaUbuntu />
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='skillsetcontent'>
+                                <h3 className='third__title'>Sonstige</h3>
+                                <a className='skillicon' href="https://www.mysql.com/de/">
+                                    <DiMysql />
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <h2 className='second__title'>Hobbies</h2>
+                <h3 className='third__title'>programmieren</h3>
+                <img 
+                    className='programm__bild'
+                    src="/images/programmieren.jpg"
+                    alt="hund"
+                />
+                <h3 className='third__title'>mein Hund</h3>
+                <img 
+                    className='hobbie__bild'
+                    src="/images/rocky.jpg"
+                    alt="hund"
+                />
+                <h3 className="third__title">Tennis</h3>
+                <img 
+                    className='hobbie__bild'
+                    src="/images/tennis.jpg"
+                    alt="tennis"
+                />
+                <h3 className='third__title'>Mountainbiken</h3>
+                <img 
+                    className='hobbie__bild'
+                    src="/images/bike.jpg"
+                    alt="bike"
+                />
+                <h3 className='third__title'>Reisen</h3>
+                <img 
+                    className='hobbie__bild'
+                    src="/images/strand.jpg"
+                    alt="strand"
+                />
+             </div>
             <h2 className='second__title'>Kontakt</h2>
             <p className='about__text'>
             Bei Fragen oder Anregungen können Sie mir gerne  <a className='link' href="mailto:sfritzsche148@gmail.com">eine E-Mail schreiben.</a>
